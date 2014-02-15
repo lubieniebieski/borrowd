6a67c649a17629f094f763979c125ef24d457238
Adam Nowak <adam.nowak@netguru.pl>
2014-02-15 15:36:36 +0100

    authenticated controller with gon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
    root 'application#start'
+   get 'app' => 'authenticated#main_app'
 
    get '/auth/:provider/callback' => 'sessions#create'
    get '/signin' => 'sessions#new', as: :signin
